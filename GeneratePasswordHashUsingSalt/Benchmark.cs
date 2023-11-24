f3d004de39717b83f2aa0416adad323613ebc819
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2023-11-24 18:28:48 +0300

    Updated methods and added Benchmark to measure performance

---
+++ GeneratePasswordHashUsingSalt/Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePasswordHashUsingSalt
+{
+    [MemoryDiagnoser()]
+    public class Benchmark
+    {
+        readonly string password = "Password";
+        readonly byte[] salt = new byte[32];
+
+        [Benchmark]
+        public void OldImplementation()
+        {
+            Program.GeneratePasswordHashUsingSalt(password, salt);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void NewImplementation()
+        {
+            Program.GeneratePasswordHashUsingSaltUpdated(password, salt);
+        }
+    }
+}
--- GeneratePasswordHashUsingSalt/Program.cs
+++ GeneratePasswordHashUsingSalt/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using BenchmarkDotNet.Running;
+using System.Security.Cryptography;
 
 namespace GeneratePasswordHashUsingSalt
 {
@@ -9,22 +10,53 @@ namespace GeneratePasswordHashUsingSalt
             string password = "Password";
             var array = new byte[32];
 
-            while(true)
+            var updatedHash = GeneratePasswordHashUsingSaltUpdated(password, array);
+            var originalHash = GeneratePasswordHashUsingSalt(password, array);
+
+            if(!updatedHash.Equals(originalHash))
             {
-                GeneratePasswordHashUsingSalt(ref password, ref array);
+                throw new Exception("They are not equal!");
             }
+            else
+            {
+                Console.WriteLine("Methods are equal!");
+            }
+
+            BenchmarkRunner.Run<Benchmark>();
         }
 
-        public static string GeneratePasswordHashUsingSalt(ref string passwordText, ref byte[] salt)
+        public static string GeneratePasswordHashUsingSaltUpdated(string passwordText, byte[] salt)
         {
 
             var iterate = 10000;
-            byte[] hash;
+            var saltSpan = salt.AsSpan();
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(passwordText.AsSpan(), saltSpan, iterate, HashAlgorithmName.SHA1, 20);
 
-            using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
+
+            Span<byte> hashBytes = stackalloc byte[36];
+
+            for (int i = 0; i < 16; i++)
             {
-                hash = pbkdf2.GetBytes(20);
+                hashBytes[i] = saltSpan[i];
             }
+
+            for (int i = 16; i < 36; i++)
+            {
+                hashBytes[i] = hash[i - 16];
+            }
+
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+        }
+
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
 
             byte[] hashBytes = new byte[36];
             Array.Copy(salt, 0, hashBytes, 0, 16);
